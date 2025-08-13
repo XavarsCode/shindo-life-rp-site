0aa773d472baf5b77eaee82dbea5183d840e4319
XavarsCode <138240303+XavarsCode@users.noreply.github.com>
2025-08-13 21:22:11 +0200

    Create login.js

---
+++ api/login.js
@@ -0,0 +1,13 @@
+// api/login.js
+const { URLSearchParams } = require('url');
+
+module.exports = (req, res) => {
+  const params = new URLSearchParams({
+    client_id: process.env.DISCORD_CLIENT_ID,
+    response_type: 'code',
+    redirect_uri: process.env.DISCORD_REDIRECT_URI,
+    scope: 'identify guilds',
+  });
+
+  res.redirect(`https://discord.com/oauth2/authorize?${params.toString()}`);
+};
